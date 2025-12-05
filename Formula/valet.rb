dcfbb8172ccefbadd89fe0bf7b8063f7a0a38429
Dong Wang <wd@wdicc.com>
2025-12-05 11:01:51 -0800

    fetch tags before build

--- Formula/valet.rb
+++ Formula/valet.rb
@@ -1,11 +1,15 @@
 class Valet < Formula
   desc "Credentials management tool for AWS SSO authentication and profile management"
   homepage "https://github.com/adRise/valet-go"
-  head "https://github.com/adRise/valet-go.git", branch: "main", shallow: false
+  head "https://github.com/adRise/valet-go.git", branch: "main"
 
   depends_on "go" => :build
 
   def install
+    if build.head?
+      system "git", "fetch", "--tags"
+    end
+
     system "make", "build"
     bin.install "bin/valet"
   end
