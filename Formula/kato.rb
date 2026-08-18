5f5a8050749b87e00f44cfca122477e822494aa2
Dong Wang <dong@tubi.tv>
2026-08-17 19:16:53 -0700

    kato: bump to v1.2.0 (#11)

--- Formula/kato.rb
+++ Formula/kato.rb
@@ -2,10 +2,10 @@ class Kato < Formula
   desc "Cloud provider access tool for Tubi engineers (successor to valet)"
   homepage "https://github.com/adRise/kato"
   url "ssh://git@github.com/adRise/kato.git",
-      tag:      "v1.1.0",
-      revision: "2172d65001938d7141540f1b067246b967e22f77",
+      tag:      "v1.2.0",
+      revision: "19aae1cb1e0c7380050e0c074101d34f238b6d99",
       using:    :git
-  version "1.1.0"
+  version "1.2.0"
 
   head "ssh://git@github.com/adRise/kato.git", branch: "main", using: :git
 
